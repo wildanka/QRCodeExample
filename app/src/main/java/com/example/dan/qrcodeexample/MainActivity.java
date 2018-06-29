2a1b24401ad635d3f9b051e2872d994dddae9eaf
Wildan Kurniadi <danprogramming10@gmail.com>
2018-06-29 23:20:38 +0700

    initial commit

---
+++ app/src/main/java/com/example/dan/qrcodeexample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.dan.qrcodeexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = (Button) findViewById(R.id.btn_scan);
+        final Activity activity = this;
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("Scan a QR Code");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.setOrientationLocked(false);
+                integrator.initiateScan();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode, data);
+
+        if (result != null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "QR Code Scan Cancelled",Toast.LENGTH_SHORT).show();
+            }{
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
